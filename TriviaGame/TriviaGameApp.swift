c2dfc35c8d0037a4229d373f4e1cfef15b364df2
Luis Bustamante <luisebt995@gmail.com>
2024-05-06 17:15:20 +0200

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Luis on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
