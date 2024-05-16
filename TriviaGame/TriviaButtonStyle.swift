44e03950a86487786a464c8d73b6802754296b8f
Luis Bustamante <luisebt995@gmail.com>
2024-05-16 18:37:45 +0200

    Generate new ButtonStyle for answer and next/finish button.

--- TriviaGame/TriviaButtonStyle.swift
+++ TriviaGame/TriviaButtonStyle.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 
 //ButtonStyle for avatar selection Button
-struct AvatarButtonStyle: ButtonStyle {
+struct AvatarButtonStyle : ButtonStyle {
     var imageName : String
     @Binding var selection : String
     
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding(55)
-            .foregroundColor(.white)
+            //.foregroundColor(.white)
             .background(
                 ZStack {
                     Image(imageName)
@@ -36,6 +36,82 @@ struct AvatarButtonStyle: ButtonStyle {
                     .onTapGesture {
                         selection = imageName
                     }
+            )
+    }
+}
+
+//ButtonStyle for answers Button
+struct AnswerButtonStyle : ButtonStyle {
+    //Defines answer: true = correct, false = incorrect
+    @State var typeAnswer : Bool
+    @State var answerNum : Int
+    
+    //Control flow variable
+    @Binding var answerControl : Int
+    @Binding var flowControl : Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            //.padding(.horizontal, 280)
+            //.padding(.vertical, 25)
+            .frame(width: 290, height: 30,alignment: .center)
+            .background(
+                ZStack {
+                    Rectangle()
+                        .frame(width: 290, height: 30)
+                        .foregroundColor(typeAnswer ? .green : .red)
+                        .opacity((flowControl && typeAnswer) || (flowControl && (answerNum == answerControl) ) ? 1 : 0)
+                        .cornerRadius(10)
+                }
+                    /*.onTapGesture {
+                        flowControl = true
+                    }*/
+            )
+    }
+}
+
+//ButtonStyle for next and finish selection Button
+struct NextButtonStyle : ButtonStyle {
+    /*Animation variable
+    @State private var animation = false*/
+    
+    //Control flow variable
+    @Binding var flowControl : Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.horizontal, 70)
+            .padding(.vertical, 10)
+            .background(
+                ZStack {
+                    Text("Next")
+                        .frame(alignment: .center)
+                        .foregroundColor(.white)
+                        .zIndex(2)
+                    Rectangle()
+                        .frame(width: 100, height: 35)
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
+                        .zIndex(1)
+                    Rectangle()
+                        .frame(width: 105, height: 40)
+                        .foregroundColor(.gray)
+                        .cornerRadius(10)
+                }
+                    .opacity(flowControl ? 1 : 0)
+                    /*.onTapGesture {
+                        flowControl = false
+                    }*/
+                    /*.scaleEffect(animation ? 0.8 : 1.0)
+                    .onTapGesture {
+                       withAnimation(Animation.spring()) {
+                            self.animation.toggle()
+                            
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            self.animation.toggle()
+                        }
+                    }*/
             )
     }
 }
